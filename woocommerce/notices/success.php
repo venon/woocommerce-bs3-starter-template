f666f8e00de3355a7f27360c1d2f3bb62e8d8f1e
venon web developers <exxxir@gmail.com>
2017-02-07 00:29:31 +0330

    first release

---
+++ woocommerce/notices/success.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Show messages
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/notices/success.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     1.6.4
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+if ( ! $messages ){
+	return;
+}
+
+?>
+<div class="alert alert-success" role="alert">
+<?php foreach ( $messages as $message ) : ?>
+	<?php
+	$message = str_replace( "button wc-forward", "wc-forward btn btn-success btn-sm", $message );
+	echo wp_kses_post( $message ); ?>
+<?php endforeach; ?>
+</div>
---
+++ woocommerce/single-product/meta.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Single Product Meta
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/meta.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     1.6.4
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+global $post, $product;
+
+$cat_count = sizeof( get_the_terms( $post->ID, 'product_cat' ) );
+$tag_count = sizeof( get_the_terms( $post->ID, 'product_tag' ) );
+
+?>
+<div class="product_meta">
+
+
+	<?php do_action( 'woocommerce_product_meta_start' ); ?>
+
+	<dl class="sku_wrapper dl-horizontal">
+
+	<?php if ( wc_product_sku_enabled() && ( $product->get_sku() || $product->is_type( 'variable' ) ) ) : ?>
+	  <dt><?php _e( 'SKU:', 'woocommerce' ); ?></dt>
+	  <dd class="sku" itemprop="sku"><?php echo ( $sku = $product->get_sku() ) ? $sku : __( 'N/A', 'woocommerce' ); ?></dd>
+	<?php endif; ?>
+
+	<?php echo $product->get_categories( ', ', '<dt class="posted_in">' . _n( 'Category:', 'Categories:', $cat_count, 'woocommerce' ) . ' </dt><dd>', '</dd>' ); ?>
+
+	<?php echo $product->get_tags( ', ', '<dt class="tagged_as">' . _n( 'Tag:', 'Tags:', $tag_count, 'woocommerce' ) . '</dt><dd> ', '</dd>' ); ?>
+
+	</dl>
+	<?php do_action( 'woocommerce_product_meta_end' ); ?>
+
+</div>
